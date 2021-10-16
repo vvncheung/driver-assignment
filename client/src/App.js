775c6ea2ad7e8fd856bfe15fce83e2966695db34
Vivian Cheung <hi@viviancheung.ca>
2021-10-16 00:44:39 -0400

    add .gitignore to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
