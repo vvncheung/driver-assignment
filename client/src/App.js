6ce22db30de9e21804764731029e465adaca8ab9
Vivian Cheung <hi@viviancheung.ca>
2021-10-23 22:06:24 -0400

    implement minimal dragndrop, slightly buggish / start edit modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,5 @@
-// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// We use Route in order to define the different routes of our application
 import { Route } from "react-router-dom";
 
-// We import all the components we need in our app
 import Navbar from "./components/navbar";
 import Edit from "./components/edit";
 import Create from "./components/create";
