842a9416a06575db28b3118dfb4a8e81e76f30e1
Vivian Cheung <hi@viviancheung.ca>
2021-10-25 05:51:47 -0400

    Update record.js

--- server/routes/record.js
+++ server/routes/record.js
@@ -15,7 +15,7 @@ const ObjectId = require("mongodb").ObjectId;
 recordRoutes.route("/record").get((req, res) => {
   let db_connect = dbo.getDb("driverDataNew");
   db_connect
-    .collection("backup")
+    .collection("driverdata")
     .find({})
     .toArray(function (err, result) {
       if (err) throw err;
@@ -28,7 +28,7 @@ recordRoutes.route("/record/:id").get((req, res) => {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId( req.params.id )};
   db_connect
-      .collection("backup")
+      .collection("driverdata")
       .findOne(myquery, function (err, result) {
         if (err) throw err;
         res.json(result);
@@ -43,7 +43,7 @@ recordRoutes.route("/record/add").post((req, response) => {
     driver: req.body.driver,
     records: req.body.records,
   };
-  db_connect.collection("backup").insertOne(myobj, function (err, res) {
+  db_connect.collection("driverdata").insertOne(myobj, function (err, res) {
     if (err) throw err;
     console.log("1 document added");
     response.json(res);
@@ -60,7 +60,7 @@ recordRoutes.route("/update/:id").post((req, response) => {
     },
   };
   db_connect
-    .collection("backup")
+    .collection("driverdata")
     .updateOne(myquery, newvalues, function (err, res) {
       if (err) throw err;
       console.log("1 document updated");
@@ -72,7 +72,7 @@ recordRoutes.route("/update/:id").post((req, response) => {
 recordRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId( req.params.id )};
-  db_connect.collection("backup").deleteOne(myquery, function (err, obj) {
+  db_connect.collection("driverdata").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
     response.status(obj);
